805daf841f64856974aaa98f0f0cbd70bb329656
zen <helmizain2206@gmail.com>
2026-04-01 21:32:14 +0700

    Add files via upload

---
+++ Praktikum-PBW/Praktikum 6/latihan_nilai.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Latihan Nilai Diskon</title>
+</head>
+<body>
+
+<form method="post">
+  NPM : <input type="text" name="npm"><br>
+  Nama : <input type="text" name="nama"><br>
+  Prodi : <input type="text" name="prodi"><br>
+  Semester : <input type="number" name="semester"><br>
+  Biaya UKT : <input type="number" name="BUKT"><br>
+  <input type="submit" value="Proses"><br><br>
+
+<?php
+if (isset($_POST['BUKT'])) {
+
+  $npm = $_POST['npm'];
+  $nama = $_POST['nama'];
+  $prodi = $_POST['prodi'];
+  $semester = $_POST['semester'];
+  $bukt = $_POST['BUKT'];
+
+  if ($bukt >= 5000000 && $semester > 8) {
+    $diskon = 0.15;
+  } else if ($bukt >= 5000000) {
+    $diskon = 0.10;
+  } else {
+    $diskon = 0;
+  }
+
+  $potongan = $bukt * $diskon;
+  $bayar = $bukt - $potongan;
+
+  echo "NPM : $npm <br>";
+  echo "Nama : $nama <br>";
+  echo "Prodi : $prodi <br>";
+  echo "Semester : $semester <br>";
+  echo "Biaya UKT : Rp " . number_format($bukt,0,",",".") . "<br>";
+  echo "Diskon : " . ($diskon*100) . "% <br>";
+  echo "<b>Yang Harus Dibayar : Rp " . number_format($bayar,0,",",".") . "</b>";
+}
+?>
+
+</form>
+</body>
+</html>
