c341c5ddd984db94acd1c20a7c4a54cace798aec
zen <helmizain2206@gmail.com>
2026-04-24 23:20:22 +0700

    Delete Praktikum-PBW/Praktikum9 directory

--- Praktikum-PBW/Praktikum9/Studi Kasus/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$conn = new mysqli("localhost", "root", "", "pemrograman_web_contoh");
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-?>
--- Praktikum-PBW/Praktikum9/Tugas/koneksi.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$conn = new mysqli("localhost", "root", "", "toko_mobil_sport");
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-}
-
-?>
