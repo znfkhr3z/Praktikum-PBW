853a7fec001869b0446caaabc663da096fed7e71
zen <helmizain2206@gmail.com>
2026-05-06 18:51:10 +0700

    Delete Pertemuan 11 directory

--- Pertemuan 11/Studi Kasus/hapus.php
+++
@@ -1,8 +0,0 @@
-<?php
-include 'koneksi.php';
-
-$id = $_GET['id'];
-$conn->query("DELETE FROM buku WHERE ID = '$id'");
-
-header("Location: index.php");
-?>
--- Pertemuan 11/Tugas/hapus.php
+++
@@ -1,9 +0,0 @@
-<?php
-include 'koneksi.php';
-
-$id = $_GET['id'];
-
-$conn->query("DELETE FROM mobil WHERE ID='$id'");
-
-header("Location: index.php");
-?>
